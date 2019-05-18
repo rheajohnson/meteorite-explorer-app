8c6b9dbb9e80b0c55d07ee0acf9d5d15e4cb8031
rjohnson91 <ryanmjohnson@protonmail.com>
2019-05-17 22:44:59 -0600

    implementing table search

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const cors = require('cors');
-const schema = require('./schema')
-const path = require('path');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema");
+const path = require("path");
 
 const app = express();
 
 // Allow cross-origin
 app.use(cors());
- 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
 
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-    res.send('hello world')
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
   })
+);
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
 
 const PORT = process.env.PORT || 5000;
- 
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
