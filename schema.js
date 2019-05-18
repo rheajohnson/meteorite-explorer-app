82483144afc0dd05f0851f5712d583f0db1fa63c
rjohnson91 <ryanmjohnson@protonmail.com>
2019-05-17 21:58:47 -0600

    building front end

--- schema.js
+++ schema.js
@@ -1,35 +1,40 @@
-const { GraphQLObjectType, GraphQLList, GraphQLInt, GraphQLString, GraphQLSchema } = require('graphql');
-const axios = require('axios');
+const {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema
+} = require("graphql");
+const axios = require("axios");
 
 const MeteoriteLandings = new GraphQLObjectType({
-    name: 'Landings',
-    fields: () => ({
-        name: { type: GraphQLString },
-        id: { type: GraphQLInt },
-        nametype: { type: GraphQLString },
-        recclass: { type: GraphQLString },
-        mass: { type: GraphQLString },
-        id: { type: GraphQLString },
-        fall: { type: GraphQLString },
-        year: { type: GraphQLString },
-    })
-})
+  name: "Landings",
+  fields: () => ({
+    name: { type: GraphQLString },
+    id: { type: GraphQLInt },
+    nametype: { type: GraphQLString },
+    recclass: { type: GraphQLString },
+    mass: { type: GraphQLString },
+    fall: { type: GraphQLString },
+    year: { type: GraphQLString }
+  })
+});
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      landings: {
-        type: new GraphQLList(MeteoriteLandings),
-        resolve(parent, args) {
-          return axios
-            .get('https://data.nasa.gov/resource/gh4g-9sfh.json')
-            .then(res => res.data);
-        }
+  name: "RootQueryType",
+  fields: {
+    landings: {
+      type: new GraphQLList(MeteoriteLandings),
+      resolve(parent, args) {
+        return axios
+          .get("https://data.nasa.gov/resource/gh4g-9sfh.json")
+          .then(res => res.data);
       }
     }
-  });
+  }
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-  });
+  query: RootQuery
+});
