16c46eeb06c77dfe302d2b20a789c924e16c1045
rjohnson91 <ryanmjohnson@protonmail.com>
2019-05-18 00:59:19 -0600

    adding github button

---
+++ public/build/precache-manifest.3c52ccf21801e80f59189f9dea3e96b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a6d5707bcf86829c54674575551114",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be79ce4fef69513cc753",
+    "url": "/static/css/2.c87619ef.chunk.css"
+  },
+  {
+    "revision": "a27312fe304c7a63a5bd",
+    "url": "/static/css/main.cdd8a035.chunk.css"
+  },
+  {
+    "revision": "be79ce4fef69513cc753",
+    "url": "/static/js/2.233e8498.chunk.js"
+  },
+  {
+    "revision": "a27312fe304c7a63a5bd",
+    "url": "/static/js/main.4a6f0c37.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
